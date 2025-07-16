5493bf7acad1bdb54a12bb34f9e4bbcbeb562aeb
vikky <vy945530@gmail.com>
2025-07-16 14:30:34 +0530

    Fix: added Strapi task properly without submodule issue

---
+++ vikky-strapi-task/src/api/vivek/routes/vivek.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vivek router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::vivek.vivek');
---
+++ vikky-strapi-task/src/api/vivek/services/vivek.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * vivek service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::vivek.vivek');
